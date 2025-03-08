b063720899425f9a5ddeecb2bbb2cc5325e007f8
abhishek maurya <maurya63abhishek@gmail.com>
2025-03-08 08:44:55 +0530

    First commit

---
+++ Day 2/script.js
@@ -0,0 +1,43 @@
+// Q2
+
+// let a = 11;
+// let b = 22;
+// let c = a + b + a++ + b++ + ++a + ++b;
+// console.log("a=" + a);
+// console.log("b=" + b);
+// console.log("c=" + c);
+
+// Q3
+
+// let i = 0;
+// i = i++ - --i + ++i - i--;
+// console.log(i);
+
+// Q4
+// let x = true;
+// x++;
+// console.log(x);
+
+// Q5
+
+// let i = 1,
+//   j = 2,
+//   k = 3;
+
+// let m = i-- - j-- - k--;
+// console.log("i=" + i);
+// console.log("j=" + j);
+// console.log("k=" + k);
+// console.log("m=" + m);
+
+// Q6
+
+// let a = 1,
+//   b = 2;
+// console.log(--b - ++a + ++b - --a);
+
+
+// Q7
+
+let i = 11
+let j = --(j++) //it is a wrong syntax because ++ && -- doesn't apply on constant value
---
+++ day 1/script.js
@@ -0,0 +1,34 @@
+// let a = 10;
+// let b = "4";
+// console.log("the sum of ", a, " and", b, "is", a + b);
+
+// console.log(a - b);
+
+// let age = Number(prompt("what is your age"));
+// console.log(typeof(age));
+
+// swapping two Numbers
+
+// method 1
+let a = 10;
+let b = 20;
+let temp;
+
+// temp = 10;
+
+// a = b;
+// b = temp;
+
+// console.log(a, b);
+
+// method 2
+
+// a = a + b; //30
+// b = a - b; //10
+// a = a - b; //20
+// console.log(a, b);
+
+[a, b] = [b, a];
+console.log(a, b)
+
+
